348d4eb52a38773b980fd0e11f755a3656a487e5
yashitakapote <93179370+yashitakapote@users.noreply.github.com>
2022-01-15 09:52:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function new_image(get_image){
 
 
         block_image_object.scaleToWidth(block_image_width);
-        block_image_object.scaleToHeight();
+        block_image_object.scaleToHeight(block_image_height);
         block_image_object.set({
             top:block_y,
             left:block_x
